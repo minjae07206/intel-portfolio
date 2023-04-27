6cb15b9982edff13eb13b868de51d6fcffb1f974
minjaelee <minjaekor@gmail.com>
2023-04-27 22:53:26 +0900

    same as commit2

--- intel-server/server.js
+++ intel-server/server.js
@@ -5,16 +5,30 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true})) 
 var cors = require('cors');
 app.use(cors());
+const MongoClient = require('mongodb').MongoClient
+let db;
+MongoClient.connect('mongodb+srv://trainerlee:intelintern@cluster0.b2yfnnx.mongodb.net/?retryWrites=true&w=majority', function(err, client){
+    if (err) return err;
+    app.listen(8080, function(){
+        console.log('listening on 8080');
+    });
+    db = client.db('intel_intern_project'); 
 
-app.listen(8080, function(){
-    console.log('listening on 8080');
-});
+})
+
+
 
 app.get('/', function(req, res){
-    res.send('dfddf')
+    db.collection('post').find().toArray(function(err, result){
+        console.log(result);
+        res.send(result);
+    })
 })
 
 app.post('/add', function(req, res){
     console.log(req.body);
     res.send('message sent.')
+    db.collection('post').insertOne({jobTitle: req.body.jobTitle, jobDescription: req.body.jobDescription}, function(){
+        console.log('저장완료');
+    })
 })
